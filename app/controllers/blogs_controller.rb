f461d1e6ce9b7d6f5a29d7d2f81cb81cdd7fed66
Tsuchida Kazuki <lovemusic1111111@gmail.com>
2016-09-03 16:54:07 +0000

    comments_show_commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,9 +7,9 @@ class BlogsController < ApplicationController
     @users = User.all
   end
   
-  # showアククションを定義します。入力フォームと一覧を表示するためインスタンスを2つ生成します。
+  
   def show
-    @comment = @blog.comments.build
+    #@comment = @blog.comments.build
     @comments = @blog.comments
   end
   
